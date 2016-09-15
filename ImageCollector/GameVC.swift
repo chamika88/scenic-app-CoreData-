65c0c30f0218fba8d3c9f55e4d7b9fd6e3c29aa9
Chamikara Dharmasena <chamikaradharmasena@Chamikaras-MacBook-Air.local>
2016-09-14 21:10:04 -0500

    first commit

---
+++ ImageCollector/GameVC.swift
@@ -0,0 +1,35 @@
+//
+//  GameVC.swift
+//  ImageCollector
+//
+//  Created by Chamikara Dharmasena on 9/12/16.
+//  Copyright © 2016 Chamikara Dharmasena. All rights reserved.
+//
+
+import UIKit
+
+class GameVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
